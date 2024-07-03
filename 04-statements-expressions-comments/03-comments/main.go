e41f739ee6285ad0de540a77b73d0acd74be99fc
Senne De Greef <senne.degreef@student.kuleuven.be>
2024-07-03 16:50:21 +0200

    [Excercises 04 Done]

--- 04-statements-expressions-comments/03-comments/main.go
+++ 04-statements-expressions-comments/03-comments/main.go
@@ -17,6 +17,7 @@ Go executes this program using this function.
 There should be only one main file in the main package.
 Executable programs are also called "commands".
 */
+
 func main() {
 	fmt.Println("Hello Gopher!")
 }
